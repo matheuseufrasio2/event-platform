97ba485ca1b6a4dd4bf14ba2f2b8b221ad3592f1
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-06-24 21:16:03 -0300

    change styles on sidebar when the video is active

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast } from 'date-fns';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 import { formatDate } from '../utils/formatDate';
 
 interface LessonProps {
@@ -16,8 +17,12 @@ export function Lesson({
   availableAt,
   type,
 }: LessonProps) {
+  const { slug: urlSLug } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(availableAt);
   const availableDateFormatted = formatDate(availableAt);
+
+  const isActiveLesson = urlSLug === slug;
 
   return (
     <Link to={`/event/lesson/${slug}`} className="group">
@@ -25,10 +30,17 @@ export function Lesson({
         {availableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+        'bg-green-500': isActiveLesson,
+      })}
+      >
         <header className="flex justify-between items-center">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span className={classNames('text-sm  font-medium flex items-center gap-2', {
+              'text-white': isActiveLesson,
+              'text-blue-500': !isActiveLesson,
+            })}
+            >
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -38,11 +50,15 @@ export function Lesson({
               Em breve
             </span>
           )}
-          <span className={`text-xs rounded py-[0.125rem] px-2 ${type === 'live' ? 'text-green-300' : 'text-white'} border border-green-300 font-bold`}>
+          <span className={`text-xs rounded py-[0.125rem] px-2 ${type === 'live' && !isActiveLesson ? 'text-green-300' : 'text-white'} border ${isActiveLesson ? 'border-white' : 'border-green-300'} font-bold`}>
             {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
-        <strong className="text-gray200 mt-5 block">
+        <strong className={classNames('mt-5 block', {
+          'text-white': isActiveLesson,
+          'text-gray-200': !isActiveLesson,
+        })}
+        >
           {title}
         </strong>
       </div>
